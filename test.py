fda6d5ad59cf8317787dcb36c59c22db66f78e98
Saurabh Kulkarni <saurabh.kulkarni997@gmail.com>
2020-09-22 12:59:31 -0500

    added a bunch of solvable test cases

--- test.py
+++ test.py
@@ -1,18 +1,47 @@
 from funny_puzzle import *
 import sys
-
+import time
 
 if __name__ == '__main__':
+
+    starttime = time.time()
+
+    # this is necessary to print to file
     backup_stdout = sys.stdout
     sys.stdout = open('test.txt', 'w')
 
-    print_succ([8,7,6,5,4,3,2,1,0])
-    print("TEST 1 DONE")
-    solve([1, 2, 3, 4, 5, 6, 7, 0, 8])
-    solve([1,2,3,4,5,6,7,8,0])
-    print("TEST 2 DONE")
+    # these are a bunch of random test cases
+    solve([6, 4, 7, 8, 5, 0, 3, 2, 1])
+    solve([8, 7, 5, 6, 0, 4, 3, 2, 1])
+    solve([8, 2, 7, 3, 1, 6, 4, 0, 5])
+    solve([[6, 5, 7, 1, 8, 2, 3, 4, 0]])
+    solve([8, 6, 5, 7, 1, 4, 0, 2, 3])
+    solve([6, 4, 8, 2, 3, 5, 1, 7, 0])
+    solve([7, 8, 6, 5, 4, 0, 2, 3, 1])
+    solve([6, 5, 7, 4, 0, 2, 3, 1, 8])
+    solve([3, 6, 2, 5, 8, 0, 4, 7, 1])
+
+    print_succ([6, 4, 7, 8, 5, 0, 3, 2, 1])
+    print_succ([8, 7, 5, 6, 0, 4, 3, 2, 1])
+    print_succ([8, 2, 7, 3, 1, 6, 4, 0, 5])
+    print_succ([6, 5, 7, 1, 8, 2, 3, 4, 0])
+    print_succ([8, 6, 5, 7, 1, 4, 0, 2, 3])
+    print_succ([6, 4, 8, 2, 3, 5, 1, 7, 0])
+    print_succ([7, 8, 6, 5, 4, 0, 2, 3, 1])
+    print_succ([6, 5, 7, 4, 0, 2, 3, 1, 8])
+    print_succ([3, 6, 2, 5, 8, 0, 4, 7, 1])
+
+    # These are unsolvable
+    #solve([7, 2, 8, 5, 0, 4, 3, 6, 1])
+    #solve([1, 2, 3, 4, 0, 6, 7, 8, 5])
+
+
+    # these are examples from the writeup
+    print_succ([1, 2, 3, 4, 5, 0, 6, 7, 8])
     solve([4, 3, 8, 5, 1, 6, 7, 2, 0])
-    solve([1,2,3,6,7,4,5,0,8])
+    print_succ([8, 7, 6, 5, 4, 3, 2, 1, 0])
+    solve([1, 2, 3, 4, 5, 6, 7, 8, 0])
+    solve([1, 2, 3, 4, 5, 6, 7, 0, 8])
 
     sys.stdout.close()
     sys.stdout = backup_stdout
@@ -30,5 +59,8 @@ if __name__ == '__main__':
             print("Yours: " + l.strip())
             print("Ref:   " + r)
 
-    print("If this is all you see, your code is good.")
+    print("If the time below is all you see, your code is good.")
 
+    endtime = time.time()
+    print("Elapsed time was: " + str(endtime - starttime))
+    print("Ref elapsed time: 32.65")
