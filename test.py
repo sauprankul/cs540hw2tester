30dc6c276ade1789ecc11aa891c1556dea4bddbd
Saurabh Kulkarni <saurabh.kulkarni997@gmail.com>
2020-09-22 23:03:59 -0500

    now detects differences in test and ref txt file lengths

--- test.py
+++ test.py
@@ -4,11 +4,10 @@ import time
 
 if __name__ == '__main__':
 
-    starttime = time.time()
-
     # this is necessary to print to file
     backup_stdout = sys.stdout
     sys.stdout = open('test.txt', 'w')
+    starttime = time.time()
 
     # these are a bunch of random test cases
     solve([6, 4, 7, 8, 5, 0, 3, 2, 1])
@@ -19,6 +18,7 @@ if __name__ == '__main__':
     solve([7, 8, 6, 5, 4, 0, 2, 3, 1])
     solve([6, 5, 7, 4, 0, 2, 3, 1, 8])
     solve([3, 6, 2, 5, 8, 0, 4, 7, 1])
+    #solve([8, 6, 7, 2, 5, 4, 3, 0, 1])
 
     print_succ([6, 4, 7, 8, 5, 0, 3, 2, 1])
     print_succ([8, 2, 7, 3, 1, 6, 4, 0, 5])
@@ -34,14 +34,14 @@ if __name__ == '__main__':
     #solve([1, 2, 3, 4, 0, 6, 7, 8, 5])
     #solve([8, 7, 5, 6, 0, 4, 3, 2, 1])
 
-
-
     # these are examples from the writeup
     print_succ([1, 2, 3, 4, 5, 0, 6, 7, 8])
     solve([4, 3, 8, 5, 1, 6, 7, 2, 0])
     print_succ([8, 7, 6, 5, 4, 3, 2, 1, 0])
     solve([1, 2, 3, 4, 5, 6, 7, 8, 0])
     solve([1, 2, 3, 4, 5, 6, 7, 0, 8])
+
+    endtime = time.time()
 
     sys.stdout.close()
     sys.stdout = backup_stdout
@@ -54,13 +54,22 @@ if __name__ == '__main__':
     live.close()
     ref.close()
 
+    line_no = 0
     for l, r in zip(live_l, ref_l):
+        line_no += 1
         if not l == r:
             print("Yours: " + l.strip())
-            print("Ref:   " + r)
-
+            print("Ref:   " + r.strip())
+            print("Line number: " + str(line_no))
+            print("\n")
+    if not live_l:
+        print("Did you... print anything? Check test.txt")
+    elif len(live_l) < len(ref_l):
+        print("Looks like you didn't print enough stuff")
+    elif len(live_l) > len(ref_l):
+        print("Looks like you didn't printed too much stuff")
     print("If the time below is all you see, your code is good.")
 
-    endtime = time.time()
-    print("Elapsed time was: " + str(endtime - starttime) + "s")
-    print("Ref elapsed time: 3.1s")
+    print("Elapsed time was: " + str((endtime - starttime)) + "s")
+    print("Ref elapsed time: 0.58s")
+    print("Record is:        0.166s")
